954a7ba5fa3bbc4e8ad776ce302367347cf19cd0
Pranav-Sandeep-Shinde <ss36507703@gmail.com>
2024-12-27 19:23:16 +0530

    Initial Commit

--- src/components/other/Header.jsx
+++ src/components/other/Header.jsx
@@ -11,17 +11,17 @@ const Header = (props) => {
   //   setUsername(data.firstName)
   // }
 
-  const logOutUser = ()=>{
-    localStorage.setItem('loggedInUser','')
+  const logOutUser = () => {
+    localStorage.setItem('loggedInUser', '')
     props.changeUser('')
     // window.location.reload()
   }
+  console.log(props)
 
-  
   return (
     <div className='flex items-end justify-between'>
-        <h1 className='text-2xl font-medium'>Hello <br /> <span className='text-3xl font-semibold'>username 👋</span></h1>
-        <button onClick={logOutUser} className='bg-red-600 text-base font-medium text-white px-5 py-2 rounded-sm'>Log Out</button>
+      <h1 className='text-2xl font-medium'>Hello <br /> <span className='text-3xl font-semibold'>👋</span></h1>
+      <button onClick={logOutUser} className='bg-red-600 text-base font-medium text-white px-5 py-2 rounded-sm'>Log Out</button>
     </div>
   )
 }
